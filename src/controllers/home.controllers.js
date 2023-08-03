70c66ceeb293f384ebe9507019f486bb62f34217
samufigh <samufigh21@gmail.com>
2023-08-03 17:04:01 -0300

    fix: install nanoid

--- src/controllers/home.controllers.js
+++ src/controllers/home.controllers.js
@@ -1,7 +1,21 @@
+import { nanoid } from "nanoid"
+import { db } from "../database/database.connection.js"
+
 export async function shorten(req, res){
     const {url} = req.body
+    const {user} = res.locals
     try {
-        res.send("shorten")
+        const shortUrl = nanoid(6)
+
+        await db.query(`
+            INSERT INTO links 
+            (url, "shortUrl", "userId")
+            VALUES ($1, $2, $3);`, [url, shortUrl, user.id])
+
+        const response = await db.query(`
+            SELECT id, "shortUrl" FROM links WHERE "userId"=$1 AND url=$2;`, [user.id, url])
+
+        res.send(response.rows[0])
     } catch (err) {
         res.status(500).send(err.message);
     }
