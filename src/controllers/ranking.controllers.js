374dcd120a9bd235593d2e2ab26832c932452525
samufigh <samufigh21@gmail.com>
2023-08-04 11:07:31 -0300

    feat: Rota GET /ranking

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -34,7 +34,12 @@ export async function openURL(req, res) {
 
 export async function ranking(req, res){
     try {
-        res.send("ranking")
+        const ranking = await db.query(`
+            SELECT users.id, users.name, COUNT(links."url") AS "linksCount", SUM(links."visitCount") AS "visitCount"
+            FROM users
+            JOIN links ON users.id=links."userId"
+            GROUP BY users.id, users.name;`)
+        res.send(ranking.rows)
     } catch (err) {
         res.status(500).send(err.message);
     }
