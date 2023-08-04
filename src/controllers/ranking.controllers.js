3029b6d8fd983ca43638bc1c50879176305893fc
samufigh <samufigh21@gmail.com>
2023-08-04 11:22:11 -0300

    fix: função Ranking

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -38,7 +38,9 @@ export async function ranking(req, res){
             SELECT users.id, users.name, COUNT(links."url") AS "linksCount", SUM(links."visitCount") AS "visitCount"
             FROM users
             JOIN links ON users.id=links."userId"
-            GROUP BY users.id, users.name;`)
+            GROUP BY users.id, users.name
+            ORDER BY "visitCount" DESC
+            lIMIT 10;`)
         res.send(ranking.rows)
     } catch (err) {
         res.status(500).send(err.message);
