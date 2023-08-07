5f14097061be37c729338d2b770483cf7dca4852
samufigh <samufigh21@gmail.com>
2023-08-07 11:55:54 -0300

    configuração: deploy

--- src/app.js
+++ src/app.js
@@ -9,5 +9,6 @@ app.use(cors())
 app.use(router)
 
 const port = process.env.PORT || 5000
-
-app.listen(port, console.log(`Servidor rodando na porta ${port}`))
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
