9151127f05067b9aa54836f66f1a8419dec620b5
samufigh <samufigh21@gmail.com>
2023-08-07 11:50:56 -0300

    configurações para deploy

--- src/database/database.connection.js
+++ src/database/database.connection.js
@@ -1,13 +1,13 @@
-import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
+import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
 
-const { Pool } = pg;
+const { Pool } = pg
 
 const configDatabase = {
   connectionString: process.env.DATABASE_URL,
-};
+}
 
-if (process.env.MODE === "prod") configDatabase.ssl = true;
+if (process.env.NODE_ENV === "production") configDatabase.ssl = true
 
-export const db = new Pool(configDatabase);
+export const db = new Pool(configDatabase)
